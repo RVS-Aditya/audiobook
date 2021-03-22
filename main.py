0960c71130cccb5d624e3749ec5a4a376cb90e00
RVS-Aditya <78360205+RVS-Aditya@users.noreply.github.com>
2021-03-22 17:36:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #To read PDF file using Python pip install pypdf2 To Read text (Text to speech) pip install pyaudio pip install pyttsx3
 import pyttsx3
 import PyPDF2
-book = open('oop.pdf', 'rb')
+book = open('crack python exam.pdf', 'rb' or any pdf u want)
 pdfReader = PyPDF2.PdfFileReader(book)
 pages = pdfReader.numPages
 
 speaker = pyttsx3.init()
-for num in range(7, pages):
+for num in range(1, pages):
     page = pdfReader.getPage(num)
     text = page.extractText()
     speaker.say(text)
